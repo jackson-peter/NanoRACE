2228f7d2ba21bf9b37b8336a452da2581c41f8e9
jackson-peter <jackson.peter90@gmail.com>
2023-03-06 16:10:39 +0100

    added_plot_tail.R

--- workflow/scripts/extract_tails.py
+++ workflow/scripts/extract_tails.py
@@ -15,6 +15,7 @@ from colorama import Fore, Style, init
 from collections import Counter
 init(autoreset=True)
 
+#NanoRACE
 
 equalities=[("M", "A"), ("M", "C"),("R", "A"), ("R", "A"), ("W", "A"), ("W", "A"), ("S", "C"), ("S", "C"), ("Y", "C"), ("Y", "C"), 
 ("K", "G"), ("K", "G"), ("V", "A"), ("V", "C"), ("V", "G"), ("H", "A"), ("H", "C"), ("H", "T"), ("D", "A"), ("D", "G"), ("D", "T"),
@@ -32,17 +33,19 @@ REV=["R-F", "R-R", "R-N", "R-UF", "R-UUR", "R-UR", "N-F", "N-UF", "UF-F", "UR-F"
 @click.option('-d', '--debug', is_flag=True, help="for developping purposes, prints additional information")
 
 def main(inadapter, inseq, out, constant_seq="CTGAC", umi_seq="NNNNNNNNNN", adapt_seq="CTGTAGGCACCATCAAT", verbose=False, debug=False):
-
-    fields = ['read_core_id','mRNA','init_polya_start_base', 'init_polya_end_base','primer_type']
+    fields = ['read_core_id','mRNA','polya_start_base', 'polya_end_base', 'init_polya_start_base', 'init_polya_end_base','primer_type', 'polya_length', 'init_polya_length']
     dtypes= {'read_core_id': str,
     'mRNA': str,
+    'polya_start_base': int,
+    'polya_end_base':int,
     'init_polya_start_base': int,
     'init_polya_end_base': int,
-    'primer_type': str}
+    'primer_type': str,
+    'polya_length': float,
+    'init_polya_length': float}
     
     df = pd.read_csv(inadapter, delimiter = "\t", usecols=fields,dtype=dtypes)
-    print("ok")
-    
+    nb_reads_input=len(df.index) 
     
     df['readname'] = df['read_core_id'].str.split(",",n=1).str[0]
     #fq = SeqIO.to_dict(SeqIO.parse(gzip.open(inseq, "rt"),'fastq'))
@@ -54,16 +57,38 @@ def main(inadapter, inseq, out, constant_seq="CTGAC", umi_seq="NNNNNNNNNN", adap
             fq[record.id] = record.seq
 
     tqdm.pandas()
-
     df['read_seqs']=df.swifter.apply(lambda x: get_seq(fq, x.readname, x.primer_type in REV ), axis=1)
+    df['sense']= np.where(df["primer_type"].isin(FWD), "FWD", "REV")
 
-
-    df[['polytail', 'additional_tail', 'delimiter', 'dist_delim', 'umi', 'adapter', 'dist_adapter']] = df.apply(get_three_primes_parts_row, constant_seq=constant_seq, umi_seq=umi_seq, adapt_seq=adapt_seq, debug=debug, axis=1 )
+    df[['polytail', 'additional_tail', 'delimiter', 'dist_delim', 'umi', 'adapter', 'dist_adapter', 'comment']] = df.apply(get_three_primes_parts_row, constant_seq=constant_seq, umi_seq=umi_seq, adapt_seq=adapt_seq, debug=debug, axis=1 )
     df = pd.concat([df, df.apply(lambda col: get_composition(col["polytail"], "A_tail"), axis=1, result_type="expand")], axis = 1)
     df = pd.concat([df, df.apply(lambda col: get_composition(col["additional_tail"], "add_tail"), axis=1, result_type="expand")], axis = 1)
     df.drop('read_seqs', axis=1, inplace=True)
     df = df.replace(r'^\s*$', np.nan, regex=True)
     df.to_csv(out, encoding='utf-8', index=False, sep='\t', na_rep="NA")
+    nb_reads_output=len(df.index)
+    nb_unid_polyA=df['polytail'].isna().sum()
+    nb_unid_add=df['additional_tail'].isna().sum()
+    nb_unid_delim=df['delimiter'].isna().sum()
+    nb_unid_umi=df['umi'].isna().sum()
+    nb_unid_adapter=df['adapter'].isna().sum()
+
+    nb_reads_fwd=df.sense.value_counts()['FWD']
+    nb_reads_rev=df.sense.value_counts()['REV']
+
+    #Making log
+    with open(out+'.log', 'w') as outlog:
+        print(f"{Fore.BLUE}{nb_reads_input} total reads in adapter file", file=outlog)
+        print(f"{Fore.BLUE}{nb_reads_output} total reads in output file", file=outlog)
+        print(f"{Fore.BLUE}{nb_reads_fwd} forward reads", file=outlog)
+        print(f"{Fore.GREEN}{nb_reads_rev} reverse reads", file=outlog)
+        
+        print(f"{Fore.RED}{nb_unid_polyA} unidentified polyA", file=outlog)
+        print(f"{Fore.RED}{nb_unid_add} unidentified add_tail", file=outlog)
+        print(f"{Fore.RED}{nb_unid_delim} unidentified delimiter", file=outlog)
+        print(f"{Fore.RED}{nb_unid_umi} unidentified umi", file=outlog)
+        print(f"{Fore.RED}{nb_unid_adapter} unidentified adapter", file=outlog)
+
 
 
 def get_three_primes_parts_row(row, constant_seq, umi_seq, adapt_seq, debug):
@@ -72,9 +97,10 @@ def get_three_primes_parts_row(row, constant_seq, umi_seq, adapt_seq, debug):
     read_seq=row['read_seqs']
     primer_type=row['primer_type']
 
+    # if no polyA, we don't look for other infos.
     if row['init_polya_start_base'] == row['init_polya_end_base']:
         
-        return pd.Series([np.nan, np.nan, np.nan,np.nan , np.nan, np.nan, np.nan])
+        return pd.Series([np.nan, np.nan, np.nan,np.nan , np.nan, np.nan, np.nan, 'polyA_start=polyA_end'])
 
     polytail=np.nan
     additional_tail=np.nan
@@ -110,9 +136,10 @@ def get_three_primes_parts_row(row, constant_seq, umi_seq, adapt_seq, debug):
     three_p_seq_without_addtail=three_p_seq[len(additional_tail):]
 
     delimiter, adapter= three_p_seq.partition(umi)[0][len(additional_tail):], three_p_seq.partition(umi)[2][:len(adapt_seq)] 
-    #delimiter, adapter= three_p_seq_without_addtail.partition(umi)[0][len(additional_tail):], three_p_seq_without_addtail.partition(umi)[2][:len(adapt_seq)] 
+ 
+    # if delimiter is absent, the alignment went wrong and therefore we shouldn't trust the results.
     if delimiter=="":
-        return pd.Series([np.nan, np.nan, np.nan,np.nan , np.nan, np.nan, np.nan])
+        return pd.Series([np.nan, np.nan, np.nan,np.nan , np.nan, np.nan, np.nan, "no delimiter found"])
 
     
     dist_adapter=edlib.align(adapter, adapt_seq,task="path", mode='HW')["editDistance"]
@@ -186,119 +213,8 @@ def get_three_primes_parts_row(row, constant_seq, umi_seq, adapt_seq, debug):
 
         sys.exit("Assertion error for reconstructed sequence")
 
-    return pd.Series([polytail, additional_tail, delimiter, dist_delim, umi, adapter, dist_adapter])
+    return pd.Series([polytail, additional_tail, delimiter, dist_delim, umi, adapter, dist_adapter, "everything ok"])
 
-"""
-def get_three_primes_parts(inadapter, inseq, out, constant_seq="CTGAC", umi_seq="NNNNNNNNNN", adapt_seq="CTGTAGGCACCATCAAT", verbose=False, debug=False):
-    three_p_motive=constant_seq+umi_seq+adapt_seq
-
-    df = pd.read_csv(inadapter, delimiter = "\t")
-
-    read_core_id_list, mRNA_list, polytail_list, additional_tail_list,delimiter_list, dist_delim_list, umi_list, adapter_list, dist_adapter_list = [], [], [], [], [], [], [], [], []
-    Count=0
-    CountFwd=0
-    CountRev=0
-    nb_proc=0
-
-    for readid, mRNA, polyA_start, polyA_end, primer_type in tqdm(zip(df['read_core_id'], df['mRNA'], df['init_polya_start_base'], df['init_polya_end_base'], df['primer_type']), total = df.shape[0]):
-        #input("wait input\n\n")
-        if polyA_start==polyA_end:
-            continue
-        Count+=1
-
-        readname = readid.split(",")[0]
-        read_seq=get_seq(inseq, readname, primer_type in REV )
-
-        if primer_type in FWD:
-            CountFwd+=1
-            gene = read_seq[:polyA_start-1]
-            polytail = read_seq[polyA_start-1:polyA_end]
-
-            three_p_seq=read_seq[polyA_end:]
-            align = get_umi_alignment(three_p_motive, three_p_seq)
-            start_adapt=align['locations'][0][0]
-            additional_tail=read_seq[polyA_end:polyA_end+start_adapt]
-
-        elif primer_type in REV:
-            CountRev+=1
-            gene = read_seq[:len(read_seq) -polyA_end]
-            polytail = read_seq[len(read_seq) -polyA_end: len(read_seq) -polyA_start+1]
-            three_p_seq=read_seq[len(read_seq) - polyA_start+1:]            
-            align = get_umi_alignment(three_p_motive, three_p_seq)
-            start_adapt=align['locations'][0][0]
-            additional_tail=read_seq[len(read_seq) - polyA_start+1:len(read_seq) - polyA_start+1+start_adapt]            
-
-       
-        else :
-            raise Exception(f"Unknown primer type: {primer_type}")
-
-        umi = align["umi"]
-        delimiter, adapter= three_p_seq.partition(umi)[0][len(additional_tail):], three_p_seq.partition(umi)[2][:len(adapt_seq)]            
-        dist_adapter=edlib.align(adapter, adapt_seq,task="path", mode='HW')["editDistance"]
-        dist_delim=edlib.align(delimiter, constant_seq,task="path", mode='HW')["editDistance"] 
-        reconstructed_seq=gene+polytail+additional_tail+delimiter + umi + adapter
-        if debug:
-            print("")
-            print("###########")
-            print(read_seq)
-            print("gene")
-            print(gene)
-            print("polytail")
-            print(polytail)
-            print("additional_tail")
-            print(additional_tail)
-            print("delimiter")
-            print(delimiter)
-            print("umi")
-            print(umi)
-            print("adapter")
-            print(adapter)
-            print(dist_adapter)
-            print(dist_delim)
-
-        assert(reconstructed_seq in read_seq)
-
-        read_core_id_list.append(readid)
-        mRNA_list.append(mRNA)
-        polytail_list.append(polytail)
-        additional_tail_list.append(additional_tail)
-        delimiter_list.append(delimiter)
-        dist_delim_list.append(dist_delim)
-        umi_list.append(umi)
-        adapter_list.append(adapter)
-        dist_adapter_list.append(dist_adapter)        
-        
-        if verbose:
-            print(f">{readname},-")
-            print(f"{Fore.GREEN}{gene}{Fore.LIGHTYELLOW_EX}{polytail}{Fore.GREEN}{delimiter}{Fore.BLUE}{umi}{Style.RESET_ALL}{Fore.GREEN}{adapter}")
-            
-    print(Counter(dist_adapter_list))
-    print(Counter(dist_delim_list))
-    res = pd.DataFrame(
-        {'read_core_id':read_core_id_list,
-          'gene': mRNA_list, 
-          'polytail': polytail_list,
-          'additional_tail' : additional_tail_list,
-          'delimiter': delimiter_list,
-          'umi': umi_list, 
-          'adapter': adapter_list,
-          'dist_adapter_list': dist_adapter_list
-          } 
-    )
-
-    print(Count, "mRNAs")
-    print(CountFwd, "fwd")
-    print(CountRev, "rev")
-    print(f"{len(umi_list)} identified 3', with {len(set(umi_list))} unique UMIs")
-
-
-    print("Adding base composition")
-    res = pd.concat([res, res.apply(lambda col: get_composition(col["polytail"], "A-tail"), axis=1, result_type="expand")], axis = 1)
-    res = pd.concat([res, res.apply(lambda col: get_composition(col["additional_tail"], "add-tail"), axis=1, result_type="expand")], axis = 1)
-
-
-    res.to_csv(out, encoding='utf-8', index=False)
-"""
 
 def get_umi_alignment(seq_to_find, seq_to_look, task="path", mode='HW'):
     wildcard=[]
@@ -346,13 +262,12 @@ def get_composition(seq: str, col_prefix) -> pd.Series:
 
         seq = regex.sub('[^ATCG]', '', seq)
         seq_len=len(seq)
-    
-        
+
         if seq_len>0:
             res = Counter(seq)
             #print(res['A'])
             nucl_count = {col_prefix+'_A': res['A'], col_prefix+'_T': res['T'], col_prefix+'_G': res['G'], col_prefix+'_C': res['C']}
-            nucl_perc = {col_prefix+'_pct_A': res['A']/seq_len, col_prefix+'_pct_T': res['T']/seq_len, col_prefix+'_pct_G': res['G']/seq_len, col_prefix+'_pct_C': res['C']/seq_len}
+            nucl_perc = {col_prefix+'_pct_A': res['A']*100/seq_len, col_prefix+'_pct_T': res['T']*100/seq_len, col_prefix+'_pct_G': res['G']*100/seq_len, col_prefix+'_pct_C': res['C']*100/seq_len}
     
     return_val = pd.concat([pd.Series(nucl_count), pd.Series(nucl_perc)])
     
---
+++ workflow/scripts/plot_tail.R
@@ -0,0 +1,187 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+library(data.table)
+library(ggpubr)
+library(grid)
+library(gridExtra)
+
+## TO DO: Change name of script and check if ok for github
+
+######## ARGUMENTS
+args = commandArgs(trailingOnly=TRUE)
+
+#suffix_add_tail=args[1]
+suffix_add_tail=".read_info.result.merged.parts.csv"
+#dir_add_tail=args[2]
+dir_add_tail="/home/jpeter/DATA/FLEPseq/RUN08_Fleurs_v3/4_Tail"
+#sample_corresp=args[3]
+sample_corresp="/home/jpeter/DATA/FLEPseq/RUN08_Fleurs_v3/barcode_correspondances.tsv"
+
+######## \ ARGUMENTS
+
+######## FUNCTIONS
+give.n <- function(x){
+  return(c(y = -1, label = length(x)))
+  # experiment with the multiplier to find the perfect position
+}
+######## \ FUNCTIONS
+
+######## DATA IMPORT
+
+samples_infos <- fread(sample_corresp, header = F, col.names = c("code", "sample")) %>%
+  mutate(add_tail_path=file.path(dir_add_tail,paste0(code,suffix_add_tail)))
+
+nlist_add_tail <- samples_infos$add_tail_path
+
+names(nlist_add_tail) <- samples_infos$code
+
+REF_genotype <- as.character(samples_infos[1, "sample"])
+
+df_uri <- rbindlist(lapply(nlist_add_tail, fread), idcol = "code") %>%
+  left_join(samples_infos, by = "code") %>%
+  separate(read_core_id, into=c(NA, "chr", "read_start", "read_end"), sep = ",") %>%
+  mutate(U_state= case_when(
+    add_tail_pct_T>0.70 ~ "U-tail",
+    TRUE ~ "non-U"))
+
+######## \ DATA IMPORT
+
+######## URIDYLATION
+
+df_uri$tail_length <- nchar(df_uri$additional_tail)
+
+
+mRNA_pctU <- df_uri%>% group_by(sample, U_state, mRNA, .drop=FALSE) %>%
+  summarise(nb_reads=n())  %>%
+  group_by(sample, mRNA) %>%
+  mutate(total=sum(nb_reads), Percent=100*nb_reads/sum(nb_reads)) %>%
+  filter(total>=50) %>%
+  arrange(sample, mRNA)
+
+global_pctU <- df_uri %>% group_by(sample, U_state, .drop=FALSE) %>%
+  summarise(nb_reads=n())  %>%
+  group_by(sample) %>%
+  mutate(total=sum(nb_reads), Percent=100*nb_reads/sum(nb_reads))
+
+mRNA_pctU$sample <- factor(mRNA_pctU$sample, levels=as.vector(samples_infos$sample))
+global_pctU$sample <- factor(global_pctU$sample, levels=as.vector(samples_infos$sample))
+
+my_colors <- c("gray50", "darkblue", "wheat", "darkred")
+
+mRNA_Utails <- mRNA_pctU %>% filter(U_state=="U-tail")
+  
+global_Utails <- global_pctU%>% filter(U_state=="U-tail")
+
+p1 <- ggplot(mRNA_Utails, aes(x=sample, y=Percent, fill=sample)) + 
+  geom_boxplot(outlier.shape = '.', show.legend = FALSE) +
+  stat_compare_means(label = "p.format", method = "wilcox.test",ref.group = REF_genotype, size = 2.9) +
+  stat_summary(fun.data = give.n, geom = "text", fun = median, size=3) +
+  ggtitle("Distribution of genes", subtitle = paste0("Wilcoxon tests of each sample versus ", REF_genotype)) +
+  scale_fill_manual(values = my_colors) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  theme(plot.title = element_text(size = 12), plot.subtitle = element_text(size=10))
+
+p2 <- ggplot(global_Utails, aes(x=sample, y=Percent, fill=sample)) + 
+  geom_bar(stat="identity", color="black", show.legend = FALSE) +
+  ggtitle("Global percentage") +
+  scale_fill_manual(values = my_colors) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=45, hjust=1)) +
+  theme(plot.title = element_text(size = 12))
+
+
+p <- grid.arrange(p2, p1, ncol=2, nrow = 1, widths=c(3,5), heights=c(4), top = textGrob(paste("Percent of uridylation of",paste(as.character(as.vector(samples_infos$sample)), collapse=", ")),gp=gpar(fontsize=14,font=2)))
+
+ggsave(filename =file.path(dir_add_tail,"AddTail_Percent_of_uridylation.pdf"), p, width = 8, height = 4, dpi = 300)
+
+df_uri$sample <- factor(df_uri$sample, levels=as.vector(samples_infos$sample))
+
+p <- ggplot(df_uri%>%filter(U_state=="U-tail")) + geom_bar(aes(tail_length, fill=sample), color="black") +
+  facet_wrap(~sample, ncol=1, scales="free") +
+  #scale_x_continuous(limits=c(0,20), breaks=seq(0,20,by=2)) +
+  scale_fill_manual(values = my_colors) +
+  theme_bw()+
+  ggtitle("Read counts vs Utail length")
+
+p
+
+ggsave(filename =file.path(dir_add_tail,"AddTail_length_barplot.pdf"), p, width = 5, height = 6, dpi = 300)
+
+df_uri_add_tail_long <- df_uri %>% filter(U_state=="U-tail") %>%
+  pivot_longer(cols=c("add_tail_pct_A", "add_tail_pct_C", "add_tail_pct_G", "add_tail_pct_T"), names_to = "add_tail_nucl", values_to = "add_tail_pct")
+
+df_uri_add_tail_long_summ <- df_uri_add_tail_long %>%
+  group_by(sample, add_tail_nucl) %>%
+  summarise(mean_add_tail_pct=mean(add_tail_pct))
+
+df_uri_add_tail_long$sample <- factor(df_uri_add_tail_long$sample, levels=as.vector(samples_infos$sample))
+df_uri_add_tail_long_summ$sample <- factor(df_uri_add_tail_long_summ$sample, levels=as.vector(samples_infos$sample))
+
+p <- ggplot(df_uri_add_tail_long_summ, aes(x=sample, y=mean_add_tail_pct, fill=sample)) + 
+  geom_bar(stat='identity') + facet_wrap(~add_tail_nucl) +
+  scale_fill_manual(values = my_colors) +
+  theme_bw() +
+  ggtitle("additional tail base composition")
+
+p
+
+ggsave(filename =file.path(dir_add_tail,"Addtail_BaseComposition.pdf"), p, width = 5, height = 6, dpi = 300)
+
+
+p <- ggplot(df_uri_add_tail_long, aes(x=sample, y=tail_length, fill=sample)) + 
+  geom_boxplot(outlier.shape = '.') +
+  scale_fill_manual(values = my_colors) +
+  theme_bw() +
+  coord_cartesian(ylim=c(0, 25))
+
+p
+ggsave(filename =file.path(dir_add_tail,"Utail_length_bp.pdf"), p, width = 5, height = 6, dpi = 300)
+
+
+######## \URIDYLATION
+
+
+######## PolyA
+
+df_uri$polya_length_nchar <- nchar(df_uri$polytail)
+
+ggplot(df_uri %>% filter(polya_length_nchar<200,
+                         polya_length_nchar>10)) +
+  geom_density(aes(polya_length_nchar, fill=sample, color=sample), alpha=0.2,  lwd = 1) +
+  facet_wrap(~U_state, scales="free") +
+  scale_fill_manual(values = my_colors) +
+  scale_color_manual(values = my_colors) +
+  theme_bw() +
+  ggtitle("Distribution of Poly-A tail sizes", subtitle = "polyA size=number of characters PolyA")
+
+ggsave(filename =file.path(dir_add_tail,"PolyA_length_nchar.pdf"),width = 5, height = 6, dpi = 300)
+
+
+ggplot(df_uri %>% filter(polya_length_nchar<200,
+                     polya_length_nchar>10)) +
+  geom_density(aes(polya_length, fill=sample, color=sample), alpha=0.2,  lwd = 1) +
+  facet_wrap(~U_state, scales="free") +
+  scale_fill_manual(values = my_colors) +
+  scale_color_manual(values = my_colors) +
+  theme_bw() +
+  ggtitle("Distribution of Poly-A tail sizes", subtitle = "polyA size=polya_length from FLEPSeq2")
+
+ggsave(filename =file.path(dir_add_tail,"PolyA_length_polyAlength.pdf"), width = 5, height = 6, dpi = 300)
+
+ggplot(df_uri %>% filter(polya_length_nchar<200,
+                         polya_length_nchar>10)) +
+  geom_density(aes(init_polya_length, fill=sample, color=sample), alpha=0.2,  lwd = 1) +
+  facet_wrap(~U_state, scales="free") +
+  scale_fill_manual(values = my_colors) +
+  scale_color_manual(values = my_colors) +
+  theme_bw() +
+  ggtitle("Distribution of Poly-A tail sizes", subtitle = "polyA size=init_polya_length from FLEPSeq2")
+
+ggsave(filename =file.path(dir_add_tail,"PolyA_length_initPolyALength.pdf"), width = 5, height = 6, dpi = 300)
+
+
+######## \ PolyA
+
+
+
